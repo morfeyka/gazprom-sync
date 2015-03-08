c34a9cd54a8ac7c461f51e4d94aef6cb777991f2
Alex Filippov <alex.filippov@gmail.com>
2015-03-08 21:41:02 +0300

    Test tool to tune pgsql connection string

---
+++ PgTest/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace PgTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var connetion = new NpgsqlConnection(textBox1.Text))
+            {
+                try
+                {
+                    connetion.Open();
+                    if (connetion.State == ConnectionState.Open)
+                    {
+                        MessageBox.Show("Success!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+    }
+}
